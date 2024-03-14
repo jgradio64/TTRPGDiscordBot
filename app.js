0b8edffb74a2a9ead806a71ef9c0e837b3b43e94
Ben Eich <jgradio64@yahoo.com>
2024-03-13 22:59:00 -0400

    Add method and functionality to delete thread history

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ let threadCollection = null;
 
 dbClient.connect().then(() => {
     console.log("connected to db");
-    db = dbClient.db("thread-messages");
+    db = dbClient.db("test");
 })
 
 const client = new Client({ 
@@ -40,7 +40,7 @@ client.on("ready", () => {
 
 client.on("messageCreate", async (message) => { 
     if (message.author.bot) return; 
-    else if (message.content.includes("@asst")) {
+    else if (message.content.includes("@assistant")) {
 
         let result = await checkThread(db, message.member.id, message.channelId);
 
@@ -54,13 +54,15 @@ client.on("messageCreate", async (message) => {
         let oldMessages = [];
         oldMessages = buildMessageArray(result.messages);
         
-        let msg = message.content.replace("@asst", "");
+        let msg = message.content.replace("@assistant", "");
         msg = msg.trim();
 
         oldMessages.push(generateUserMessage(msg));
 
         let idk = await executeChatCompletetion(oldMessages).then(
-            res => message.reply(res.message.content)
+            res => {
+                message.reply(res.message.content);
+            }
         ).catch(
             err => console.log(err)
         );
@@ -72,7 +74,22 @@ client.on("messageCreate", async (message) => {
         await updateThread(db, result._id, newMessages);
     }
     else if (message.mentions.has(botID)) {
-        message.reply("Yup, I'm here to help!");
+        let msg = message.content.slice(22);
+        msg = msg.trim();
+        // Mentioned ID is 22 places
+        
+
+        if(msg.toLowerCase() == "delete gpt history") {
+            message.reply("Attempting to delete conversation history!");
+            // delete the mongoDB history for the user in the channel
+            let deletionResult = await deleteThread(db, message.author.id, message.channelId);
+            if(deletionResult.acknowledged && deletionResult.deletedCount != 0) {
+                message.reply("History has been successfully deleted!");
+            } else {
+                message.reply("There has been an error with your deletion request.")
+            }
+        }
+
     }
 });
 
@@ -115,6 +132,15 @@ async function createDefaultThread(userID, channelID){
     defaultThread._id = result.insertedId;
 
     return defaultThread;
+}
+
+async function deleteThread(database, userid, channelid) {
+    let threadCollection = database.collection("gpt-threads");
+    let result = await threadCollection.deleteOne({
+        userID: userid,
+        channelID: channelid
+    });
+    return result;
 }
 
 function generateUserMessage(message) {
