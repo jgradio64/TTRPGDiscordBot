d75070b08b64664cddb24351b02d37267cc236a8
Ben Eich <jgradio64@yahoo.com>
2024-05-14 13:33:54 -0400

    Change it so that the user always has to tag the bot to run commands

--- app.js
+++ app.js
@@ -43,74 +43,11 @@ client.on("ready", () => {
 
 client.on("messageCreate", async (message) => {
     if (message.author.bot) return;
-    else if (message.content.includes("@assistant")) {
-
-        let mongoThread = await checkThread(db, message.member.id, message.channelId);
-
-        if (mongoThread == null) {
-            // Create the default thread, and then return the created object 
-            // update the new thread after querying chatGPT
-            mongoThread = await createDefaultThread(db, message.member.id, message.channelId);
-        }
-
-        // Array of old messages is created to send history of conversation to chat GPT
-        let oldMessages = [];
-        // Array of new messages is used to update the database record for the conversation.
-        let newMessages = [];
-
-        oldMessages = buildMessageArray(mongoThread.messages);
-
-        let msg = message.content.replace("@assistant", "");
-        msg = msg.trim();
-
-        oldMessages.push(generateUserMessage(msg));
-        newMessages.push(generateUserMessage(msg))
-
-        // Used to use the result of the function call here, but unused right now.
-        let result = await executeChatCompletetion(oldMessages).then(
-            res => {
-                let text = res.message.content;
-
-                // Push the assistant's message to the database before splitting it. 
-                newMessages.push(generateAssistantMessage(text))
-
-                /*
-                    Discord.js cannot send a message reply with more than 2000 characters.
-                    So loop through the GPT reply and split the message into multiple replies if we need.
-                */
-
-                do {
-                    if (text.length > 2000) {
-                        // Get the last space before the 2000th index in the string, a word isn't cut in half for no reason.
-                        let n = text.lastIndexOf(" ", 2000);
-
-                        if (n === -1) {
-                            // If for some reason there are no spaces at all, just cut the string at the 2000th index space and send that.
-                            // If this ever pops up I need to revisit this.
-                            n = 2000;
-                        }
-
-                        message.reply(text.slice(0, n));
-                        text = text.slice(n);
-                    } else {
-                        // Execute final reply and make the string an empty string. 
-                        message.reply(text);
-                        text = "";
-                    }
-                } while (text.length > 0);
-            }
-        ).catch(
-            err => console.log(err)
-        );
-
-        await updateThread(db, mongoThread._id, newMessages);
-    }
     else if (message.mentions.has(botID)) {
+
         // The ID for a Mentioned user is 22 places long
         let msg = message.content.slice(22);
         msg = msg.trim();
-
-
         if (msg.toLowerCase() == "delete") {
             message.reply("Attempting to delete conversation history!");
             // delete the mongoDB history for the user in the channel
@@ -121,6 +58,67 @@ client.on("messageCreate", async (message) => {
             } else {
                 message.reply("There has been an error with your deletion request.")
             }
+        } else {
+            let mongoThread = await checkThread(db, message.member.id, message.channelId);
+
+            if (mongoThread == null) {
+                // Create the default thread, and then return the created object 
+                // update the new thread after querying chatGPT
+                mongoThread = await createDefaultThread(db, message.member.id, message.channelId);
+            }
+
+            // Array of old messages is created to send history of conversation to chat GPT
+            let oldMessages = [];
+            // Array of new messages is used to update the database record for the conversation.
+            let newMessages = [];
+
+            oldMessages = buildMessageArray(mongoThread.messages);
+
+            let msg = message.content.replace("@assistant", "");
+            msg = msg.trim();
+
+            oldMessages.push(generateUserMessage(msg));
+            newMessages.push(generateUserMessage(msg))
+
+            // Used to use the result of the function call here, but unused right now.
+            let result = await executeChatCompletetion(oldMessages).then(
+                res => {
+                    let text = res.message.content;
+
+                    // Push the assistant's message to the database before splitting it. 
+                    newMessages.push(generateAssistantMessage(text))
+
+                    /*
+                        Discord.js cannot send a message reply with more than 2000 characters.
+                        So loop through the GPT reply and split the message into multiple replies if we need.
+                    */
+
+                    do {
+                        if (text.length > 2000) {
+                            // Get the last space before the 2000th index in the string, a word isn't cut in half for no reason.
+                            let n = text.lastIndexOf(" ", 2000);
+
+                            if (n === -1) {
+                                // If for some reason there are no spaces at all, just cut the string at the 2000th index space and send that.
+                                // If this ever pops up I need to revisit this.
+                                n = 2000;
+                            }
+
+                            message.reply(text.slice(0, n));
+                            text = text.slice(n);
+                        } else {
+                            // Execute final reply and make the string an empty string. 
+                            message.reply(text);
+                            text = "";
+                        }
+                    } while (text.length > 0);
+                }
+            ).catch(
+                err => console.log(err)
+            );
+
+            await updateThread(db, mongoThread._id, newMessages);
+
         }
 
     }
